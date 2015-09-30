774f09bc3537505b93ba5b4984414dd8253d9bfc
Fabian Wüthrich <fabian.wuethrich@outlook.com>
2015-09-30 13:04:54 +0200

    Improve login

--- php/classes/ChatUser.class.php
+++ php/classes/ChatUser.class.php
@@ -1,29 +1,52 @@
 <?php
 
-class ChatUser extends ChatBase{
-	
-	protected $name = '', $gravatar = '';
-	
-	public function save(){
-		
-		DB::query("
-			INSERT INTO webchat_users (name, gravatar)
+class ChatUser extends ChatBase
+{
+
+    private $name = '';
+    private $gravatar = '';
+    private $email = '';
+    private $password = '';
+
+    public static function get($name)
+    {
+        $result = DB::query("SELECT * FROM webchat_users WHERE name = '" . DB::esc($name) . "'");
+
+        $user = $result->fetch_object();
+
+        return $user;
+    }
+
+    public function save()
+    {
+        DB::query("
+			INSERT INTO webchat_users (name, gravatar, email, password)
 			VALUES (
-				'".DB::esc($this->name)."',
-				'".DB::esc($this->gravatar)."'
+				'" . DB::esc($this->name) . "',
+				'" . DB::esc($this->gravatar) . "'
+				'" . DB::esc($this->email) . "'
+				'" . DB::esc($this->password) . "'
 		)");
-		
-		return DB::getMySQLiObject();
-	}
-	
-	public function update(){
-		DB::query("
-			INSERT INTO webchat_users (name, gravatar)
+
+        return DB::getMySQLiObject();
+    }
+
+    public function update()
+    {
+        DB::query("
+			INSERT INTO webchat_users (name, gravatar, email, password)
 			VALUES (
-				'".DB::esc($this->name)."',
-				'".DB::esc($this->gravatar)."'
+	            '" . DB::esc($this->name) . "',
+				'" . DB::esc($this->gravatar) . "'
+				'" . DB::esc($this->email) . "'
+				'" . DB::esc($this->password) . "'
 			) ON DUPLICATE KEY UPDATE last_activity = NOW()");
-	}
+    }
+
+    public function delete()
+    {
+        DB::query("DELETE FROM webchat_users WHERE name = '" . DB::esc($this->name) . "'");
+    }
 }
 
 ?>
